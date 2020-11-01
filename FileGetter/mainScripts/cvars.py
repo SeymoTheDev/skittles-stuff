26d86a9e0b49dfafc96c3abb98eef289486277df
SeymoTheDev <72935852+SeymoTheDev@users.noreply.github.com>
2020-11-01 14:24:33 -0500

    python logging systems

---
+++ FileGetter/mainScripts/cvars.py
@@ -0,0 +1,6 @@
+cvar = 'cvar.true'
+cvar_getfilepathDesktop = 'C:/Users/hostname/Desktop/'
+cvar_getfilepathDocuments = 'C:/Users/hostname/Documents'
+cvar_getfilepathDownloads = 'C:/Users/hostname/Downloads'
+hostname = input('Enter the name of your host machine  >')
+print('Input received. PRINT:%s'%hostname)
